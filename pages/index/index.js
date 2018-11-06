e00814ee9121da28532921cd1d8f14a91c96ea07
headB_macbook_pro <775121173@qq.com>
2018-11-06 19:05:07 +0800

    暂时先提交,但是成功获取了用户的openid信息

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,8 +6,36 @@ Page({
 
 clickMe: function(){
 this.setData({msg:"哈哈哈哈"})
+wx.login({
+  success(res) {
+    console.log("I am kumanxuan")
+    if (res.code) {
+      //发起网络请求
+      
+      //这里成功之后,就可以尝试去获取用户的openid了
+      var res_url = 'https://api.weixin.qq.com/sns/jscode2session?appid=wx2f6212c8f8f7172e&secret=e7e73777f0b044961cc47a720853b226&js_code='+res.code+'&grant_type=authorization_code'
+
+      wx.request({
+        url: res_url,
+        success(res1){
+          console.log(res1)
+          this.setData({openid:res1.data.openid})
+        }
+        // data: {
+        //   code: res.code
+        // }
+      })
+      
+
+    } else {
+      console.log('登录失败！' + res.errMsg)
+    }
+  }
+})
 },
   data: {
+    userDetailInfo: '',
+    openid: '',
     motto: '欢迎使用-狼码办公小程序',
     userInfo: {},
     hasUserInfo: false,
