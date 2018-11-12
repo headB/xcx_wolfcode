206171a1866e480147f5a91d93a7e5ef0b25bfef
headB_macbook_pro <775121173@qq.com>
2018-11-13 00:52:08 +0800

    在小程序成功利用form表单异步请求数据到后台

--- pages/verify/verify.js
+++ pages/verify/verify.js
@@ -5,7 +5,9 @@ Page({
    */
   data: {
     userRegisterInfo : '',
-    
+    isRegister: false,
+    code: null,
+    req_url: "http://127.0.0.1:5000/xcx/verify"
   },
 
   //检查用户已经是登陆用户
@@ -14,32 +16,65 @@ Page({
     //后台请求,携带code给后台
 
     var userRegisterInfo = this.data.userRegisterInfo
-    var req_url = "http://127.0.0.1:5000/xcx/verify?code="
     var _this = this
     wx.login({
       success(res) {
         if (res.code) {
-          //发起网络请求
+          _this.data.code = res.code
           wx.request({
-            url: req_url+res.code,
-            success: function(res){
-              
-              userRegisterInfo = res.data.status
-              _this.setData({
-                userRegisterInfo:userRegisterInfo
-              })
-              // this.setData({
-              //   userRegisterInfo: res.data.status
-              // })
-              
-              console.log(res)
-            }
+            url: _this.data.req_url,
+            data:{
+              'code':res.code
+            },
+            success: function (res) {
+            userRegisterInfo = res.data.status
+            _this.setData({
+            userRegisterInfo: userRegisterInfo
+            })}
           })
         } else {
           console.log('登录失败！' + res.errMsg)
         }
-      }
+    }
     })
+  },
+  formSubmit: function (e) {
+
+    
+    if (this.data.code && e.detail.value.realname){
+      //参数不为空,请求后台注册账号
+      var realname = e.detail.value.realname
+      wx.request({
+        url: this.data.req_url,
+        data:{
+          "code":this.data.code,
+          "realname":realname
+        },
+        method:"post",
+        success:function(e){
+          console.log(e)
+        }
+      })
+
+    }else{
+      wx.showModal({
+        title: '提示',
+        content: '名字不能为空',
+        showCancel: true,
+        cancelText: 'Intel NO',
+        cancelColor: '',
+        confirmText: 'AMD YES',
+        confirmColor: '',
+        success: function(res) {},
+        fail: function(res) {},
+        complete: function(res) {},
+      })
+    }
+
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+  formReset: function () {
+    console.log('form发生了reset事件')
   },
   /**
    * 生命周期函数--监听页面加载
