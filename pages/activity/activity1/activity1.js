6cb19e07b9820c890b88d805a2e293d331102c4e
headB_macbook_pro <775121173@qq.com>
2019-06-24 12:28:55 +0800

    成功设置好了新的时间选择器
    成功设置好了新的时间选择器

--- pages/activity/activity1/activity1.js
+++ pages/activity/activity1/activity1.js
@@ -3,6 +3,8 @@
 //全局变量!
 const app = getApp()
 
+var dateTimePicker = require('time.js');
+
 Page({
 
   formSubmit: function (e) {
@@ -11,14 +13,16 @@ Page({
     // console.log(app.globalData.cookie);
     var _this = this;
     console.log(_this.data);
+    var d1 = this.data.dateTime;
+    var d2 = this.data.dateTime1;
     wx.request({
       url: app.globalData.base_url+"/xcx/activity", //仅为示例，并非真实的接口地址
       method:"POST",
       data: {
         topic: e.detail.value.topic,
         name: e.detail.value.name,
-        startDate: _this.data.startDate, 
-        endDate: _this.data.endDate
+        startDate: d1[0]+2000+"-"+(d1[1]+1)+"-"+d1[2]+" "+d1[3]+":"+d1[4], 
+        endDate: d2[0] + 2000 + "-" + (d2[1] + 1) + "-" + d2[2] + " " + d2[3] + ":" + d2[4]
       },
       header: {
         'content-type': 'application/json', // 默认值
@@ -61,13 +65,35 @@ Page({
     startDate:"←点击设置开始时间(郁我阿笨)",
     endDate:"←点击设置结束时间(收皮时间)",
     topic:"",
-    name:""
+    name:"",
+    date: '2018-10-01',
+    time: '12:00',
+    dateTimeArray: null,
+    dateTime: null,
+    dateTimeArray1: null,
+    dateTime1: null,
+    startYear: 2000,
+    endYear: 2050
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+
+    // 获取完整的年月日 时分秒，以及默认显示的数组
+    var obj = dateTimePicker.dateTimePicker(this.data.startYear, this.data.endYear);
+    var obj1 = dateTimePicker.dateTimePicker(this.data.startYear, this.data.endYear);
+    // 精确到分的处理，将数组的秒去掉
+    var lastArray = obj1.dateTimeArray.pop();
+    var lastTime = obj1.dateTime.pop();
+
+    this.setData({
+      dateTime: obj.dateTime,
+      dateTimeArray: obj.dateTimeArray,
+      dateTimeArray1: obj1.dateTimeArray,
+      dateTime1: obj1.dateTime
+    });
 
   },
 
@@ -118,5 +144,44 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  changeDate(e) {
+    this.setData({ date: e.detail.value });
+  },
+  changeTime(e) {
+    this.setData({ time: e.detail.value });
+  },
+  changeDateTime(e) {
+    this.setData({ dateTime: e.detail.value });
+  },
+  changeDateTime1(e) {
+    this.setData({ dateTime1: e.detail.value });
+  },
+  changeDateTimeColumn(e) {
+    var arr = this.data.dateTime, dateArr = this.data.dateTimeArray;
+
+    arr[e.detail.column] = e.detail.value;
+    dateArr[2] = dateTimePicker.getMonthDay(dateArr[0][arr[0]], dateArr[1][arr[1]]);
+
+    this.setData({
+      dateTimeArray: dateArr,
+      dateTime: arr
+    });
+  },
+  changeDateTimeColumn1(e) {
+    var arr = this.data.dateTime1, dateArr = this.data.dateTimeArray1;
+
+    arr[e.detail.column] = e.detail.value;
+    dateArr[2] = dateTimePicker.getMonthDay(dateArr[0][arr[0]], dateArr[1][arr[1]]);
+
+    this.setData({
+      dateTimeArray1: dateArr,
+      dateTime1: arr
+    });
   }
+
+
+
+
 })
